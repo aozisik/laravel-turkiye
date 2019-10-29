d3a514d1f6243d445f9e3439e041b7ed1fbd5939
Ahmet Özışık <ozisikahmet@gmail.com>
2019-10-29 11:21:13 +0200

    İngilizce hata mesajlarını ekle

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'tckn' => ':attribute is incomplete or invalid.',
+
+    'vkn' => ':attribute is incomplete or invalid.',
+
+    'tr_iban' => ':attribute is incomplete or invalid.',
+
+];
